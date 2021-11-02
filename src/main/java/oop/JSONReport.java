13f072ea0890883567a56d8518099dd47134b780
Exsnipe <exsnipe@yandex.ru>
2021-11-02 07:27:53 -0700

    Аннотация @Override [#174891]

--- src/main/java/oop/JSONReport.java
+++ src/main/java/oop/JSONReport.java
@@ -1,6 +1,7 @@
 package oop;
 
 public class JSONReport extends TextReport {
+    @Override
     public String generate(String name, String body) {
         return "{" + System.lineSeparator() + "\"" + name + "\"" + " : " + "\"" + name
                 + "\"" + System.lineSeparator() + "\"" + body + "\"" + " : " + "\"" + body
