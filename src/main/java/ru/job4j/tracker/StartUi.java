74e1f9c5eb784ad9c4497212565237af09ea2002
Exsnipe <exsnipe@yandex.ru>
2021-11-06 07:18:04 -0700

    Реализация класса StartUI. Вывод меню [#500743]

--- src/main/java/ru/job4j/tracker/StartUi.java
+++ src/main/java/ru/job4j/tracker/StartUi.java
@@ -1,15 +1,38 @@
 package ru.job4j.tracker;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
+import java.util.Scanner;
 
 public class StartUi {
+
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.println("Select: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("You have  chose: " + select + System.lineSeparator());
+            } else {
+                run = false;
+            }
+        }
+    }
+
+    public void showMenu() {
+        String[] menuItems = {
+            "Add new Item", "Show all items", "Edit item",
+            "Delete item", "Find item by id", "Find items by name",
+            "Exit Program"
+        };
+        System.out.println("Menu:");
+        for (int i = 0; i < menuItems.length; i++) {
+            System.out.println(i + ". " + menuItems[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        Item item = new Item();
-        LocalDateTime  created = item.getCreated();
-        DateTimeFormatter formater = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String dateTimeFormated = created.format(formater);
-        System.out.println(dateTimeFormated);
-        Item newItem = new Item(1234, "New Item");
-        System.out.println(newItem);
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUi().init(scanner, tracker);
     }
 }
