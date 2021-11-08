e0cd2de7d7bc6cefdc06eb458f6ae3b902ebf416
Exsnipe <exsnipe@yandex.ru>
2021-11-08 06:58:55 -0800

    Scanner и чтение числа из консоли. [#179518 #225007]

--- src/main/java/io/Matches.java
+++ src/main/java/io/Matches.java
@@ -7,23 +7,22 @@ public class Matches {
         System.out.println("Start game 11");
         Scanner input = new Scanner(System.in);
         int count = 11;
-        boolean turn = false;
+        boolean turn = true;
         while (count > 0) {
-            System.out.println("First player takes:");
-            count -= Integer.parseInt(input.nextLine());
-            if (count <= 0) {
+            String player = turn ? "Первый игрок" : "Второй игрок";
+            System.out.println(player + " введите число от 1 до 3:");
+            int matches = Integer.parseInt(input.nextLine());
+            if (matches > 0 && matches < 4) {
                 turn = !turn;
-                break;
+                System.out.println(player + " ввел " + matches);
+                count -= matches;
+                System.out.println("Остаток: " + Math.max(0, count));
             }
-            System.out.println("Осталось " + count);
-            System.out.println("Second player takes");
-            count -= Integer.parseInt(input.nextLine());
-            if (count <= 0) {
-                break;
-            }
-            System.out.println("Осталось " + count);
         }
-        String rsl = !turn ? "Second" : "First";
-        System.out.println(rsl + " player wone");
+        if (!turn) {
+            System.out.println("Выиграл первый игрок");
+        } else {
+            System.out.println("Выиграл второй игрок");
+        }
     }
 }
