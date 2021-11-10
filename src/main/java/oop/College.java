6216ae2497da1ecc718117852e3a8c4664833f77
Exsnipe <exsnipe@yandex.ru>
2021-11-10 08:03:21 -0800

    Приведение типов. Повышение типа и понижение типа. [#181322 #226872]

--- src/main/java/oop/College.java
+++ src/main/java/oop/College.java
@@ -2,8 +2,8 @@ package oop;
 
 public class College {
     public static void main(String[] args) {
-        Freshman frshmn = new Freshman();
-        Student sFrsmn = frshmn;
-        Object oSFrshmn = sFrsmn;
+        Freshman freshman = new Freshman();
+        Student sFreshman = freshman;
+        Object oFreshman = sFreshman;
     }
 }
