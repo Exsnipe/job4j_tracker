73cda85d6e0a88aa1a0d3e61017a9880f24a89f9
Exsnipe <exsnipe@yandex.ru>
2021-11-17 08:32:20 -0800

    Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUiTest.java
+++ src/test/java/ru/job4j/tracker/StartUiTest.java
@@ -7,7 +7,7 @@ import static org.hamcrest.core.Is.is;
 public class StartUiTest {
 
     @Test
-    public void addNewItem() {
+    public void whenAddNewItem() {
         String[] answers = {"Fix PC"};
         Input stbInput = new StubInput(answers);
         Tracker tracker = new Tracker();
@@ -15,5 +15,28 @@ public class StartUiTest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
+    }
+
+    @Test
+    public void whenEdited() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("New item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()), "edited item"};
+        StartUi.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName(), is("edited item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("New item");
+        tracker.add(item);
+        int curId = item.getId();
+        String[] answers = {String.valueOf(curId)};
+        StartUi.deleteItem(new StubInput(answers), tracker);
+        Item nullPointer = null;
+        assertThat(tracker.findById(curId), is(nullPointer));
     }
 }
