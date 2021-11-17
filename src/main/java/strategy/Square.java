cfd5f07f578563efbb5d0d7a32aed923ceacc299
Exsnipe <exsnipe@yandex.ru>
2021-11-17 10:25:26 -0800

    Шаблон проектирования - Стратегия [#786]

---
+++ src/main/java/strategy/Square.java
@@ -0,0 +1,17 @@
+package strategy;
+
+public class Square implements Shape {
+    @Override
+    public String draw() {
+        String ln = System.lineSeparator();
+        return "--------------" + ln
+            + "|            |" + ln
+            + "|            |" + ln
+            + "|            |" + ln
+            + "--------------";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Square().draw());
+    }
+}
