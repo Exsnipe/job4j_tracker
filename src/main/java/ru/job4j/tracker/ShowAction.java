7e67d14d7e6defdfc8d44d6f15b74e92b8512a02
Exsnipe <exsnipe@yandex.ru>
2021-11-20 20:26:15 -0800

    Зависимость от System.out [#33568 #230008]

--- src/main/java/ru/job4j/tracker/ShowAction.java
+++ src/main/java/ru/job4j/tracker/ShowAction.java
@@ -14,14 +14,14 @@ public class ShowAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        System.out.println("=== Show all items ===");
+        out.println("=== Show all items ===");
         Item[] allFiles = tracker.findAll();
         if (allFiles.length > 0) {
             for (Item item : allFiles) {
-                System.out.println(item);
+                out.println(item);
             }
         } else {
-            System.out.println("There are no items in storage");
+            out.println("There are no items in storage");
         }
         return true;
     }
