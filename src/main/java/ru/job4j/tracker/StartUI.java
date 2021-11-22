c091d567a71f4b87399bdf3541f6dbeb8d95bae2
Exsnipe <exsnipe@yandex.ru>
2021-11-21 22:23:46 -0800

    Обеспечить бесперебойную работу приложения Tracker [#789]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,6 +12,10 @@ public class StartUI {
         while (run) {
             showMenu(action);
             int select = input.askInt("select:");
+            if (select < 0 || select > action.length - 1) {
+                out.println("Wrong input. You can select 0 .. " + (action.length - 1));
+                continue;
+            }
             run = action[select].execute(input, tracker);
         }
     }
@@ -25,7 +29,7 @@ public class StartUI {
 
     public static void main(String[] args) {
         Output out = new ConsoleOutput();
-        Input input = new ConsoleInput();
+        Input input = new ValidateInput();
         Tracker tracker = new Tracker();
         UserAction[] action = {new CreateAction(out), new ShowAction(out), new EditAction(out),
         new DeleteAction(out), new FindByIdAction(out), new FindByNameAction(out),
