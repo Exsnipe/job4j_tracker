59d6385875e5447b89d6b0a2bfc548afafc3858f
Exsnipe <exsnipe@yandex.ru>
2021-11-22 20:02:17 -0800

    Error - исключения, связанные с работой виртуальной машины [#219370 #231390]

--- src/main/java/ex/FactRec.java
+++ src/main/java/ex/FactRec.java
@@ -4,9 +4,8 @@ public class FactRec {
     public static int factRec(int n) {
         if (n == 1) {
             return 1;
-        } else {
-            return factRec(n - 1) * n;
         }
+        return factRec(n - 1) * n;
     }
 
     public static void main(String[] args) {
