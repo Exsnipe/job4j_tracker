c987e53bdd96f68e5376fd63322e28af842c3b05
Exsnipe <exsnipe@yandex.ru>
2021-11-29 22:50:09 -0800

    Банковские переводы [#10038]

---
+++ src/main/java/bank/BankService.java
@@ -0,0 +1,60 @@
+package bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        if (user != null && !users.containsKey(user)) {
+            users.put(user, new ArrayList<>());
+        }
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> list = users.get(user);
+            if (!list.contains(account)) {
+                list.add(account);
+            }
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> list = users.get(user);
+            for (Account account : list) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite, String destPassport,
+                                 String destRequisite, double amount) {
+        Account outCome = findByRequisite(srcPassport, srcRequisite);
+        Account inCome = findByRequisite(destPassport, destRequisite);
+        if (outCome != null && inCome != null && outCome.getBalance() >= amount) {
+            outCome.setBalance(outCome.getBalance() - amount);
+            inCome.setBalance(inCome.getBalance() + amount);
+            return true;
+        }
+        return false;
+    }
+}
