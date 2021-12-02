f58ff61851bb753935ee8c191da0c889f0e8afa2
Exsnipe <exsnipe@yandex.ru>
2021-12-01 22:39:38 -0800

    Организовать сортировку User [#10034]

---
+++ src/main/java/collection/User.java
@@ -0,0 +1,46 @@
+package collection;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int compareTo(User another) {
+        if (name.equals(another.name)) {
+            return Integer.compare(age, another.age);
+        }
+        return name.compareTo(another.name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return age == user.age && name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
