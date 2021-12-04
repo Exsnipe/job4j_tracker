7076781ae8b494f9314cd099b7fe81826d03d195
Exsnipe <exsnipe@yandex.ru>
2021-12-03 22:02:30 -0800

    Компаратор для строк [#35008]

---
+++ src/main/java/collection/StringComparator.java
@@ -0,0 +1,17 @@
+package collection;
+
+import java.util.Comparator;
+
+public class StringComparator implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int index = Math.min(left.length(), right.length());
+        for (int i = 0; i < index; i++) {
+            int rsl = Character.compare(left.charAt(i), right.charAt(i));
+            if (rsl != 0) {
+                return rsl;
+            }
+        }
+        return Integer.compare(left.length(), right.length());
+    }
+}
