8e73f873abee580645a020603d9c2ec88069ffe0
Exsnipe <exsnipe@yandex.ru>
2021-12-03 17:51:21 -0800

    Комбинированный компаратор [#10036]

--- src/test/java/collection/JobTest.java
+++ src/test/java/collection/JobTest.java
@@ -10,7 +10,8 @@ import static org.junit.Assert.assertThat;
 public class JobTest {
     @Test
     public void whenCompatorByNameAndPrority() {
-        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        Comparator<Job> cmpNamePriority =
+                new JobDescByName().thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 0),
                 new Job("Fix bug", 1)
@@ -60,7 +61,8 @@ public class JobTest {
 
     @Test
     public void whenCompareAscByPriorityAscByName() {
-        Comparator<Job> comparePriorityAndName = new JobAscByPriority().thenComparing(new JobAscByName());
+        Comparator<Job> comparePriorityAndName =
+                new JobAscByPriority().thenComparing(new JobAscByName());
         int rsl = comparePriorityAndName.compare(
                 new Job("Beta", 1),
                 new Job("Alpha", 1)
