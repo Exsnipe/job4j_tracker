6dd2565ee2444eb765e00adfdf5092a5651c82f1
Exsnipe <exsnipe@yandex.ru>
2021-12-08 20:41:04 -0800

    Ссылки на методы [#70608]

---
+++ src/main/java/lambda/RefMethod.java
@@ -0,0 +1,26 @@
+package lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class RefMethod {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(Arrays.asList(
+                "Ivan",
+                "Petr Arsentev"
+        ));
+        Consumer<String> cons = RefMethod::cutOut;
+        list.forEach(cons);
+
+    }
+
+    public static void cutOut(String value) {
+        if (value.length() > 10) {
+            System.out.println(value.substring(0, 10) + "..");
+        } else {
+            System.out.println(value);
+        }
+    }
+}
