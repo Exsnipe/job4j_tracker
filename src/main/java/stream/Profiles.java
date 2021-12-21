14543af61420d802bd37781537605637b382a020
Exsnipe <exsnipe@yandex.ru>
2021-12-20 22:14:07 -0800

    Уникальность элементов и сортировка. [#110225 #242347]

--- src/main/java/stream/Profiles.java
+++ src/main/java/stream/Profiles.java
@@ -6,7 +6,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-    private Comparator<Address> comparator = (Address o1, Address o2) -> o1.getCity().compareTo(o2.getCity());
+    private Comparator<Address> comparator = (Address o1, Address o2) -> o1.getCity()
+            .compareTo(o2.getCity());
 
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(profile -> profile.getAddress()).collect(Collectors.toList());
