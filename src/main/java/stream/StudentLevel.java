262a4d77861ebd2d4911e8b6daeee70eb775a33d
Exsnipe <exsnipe@yandex.ru>
2021-12-24 21:57:14 -0800

    Stream API улучшения [#58167 #243486]

--- src/main/java/stream/StudentLevel.java
+++ src/main/java/stream/StudentLevel.java
@@ -9,7 +9,7 @@ public class StudentLevel {
     public static List<Stud> levelOf(List<Stud> students, int bound) {
         return students.stream()
                 .filter(Objects::nonNull)
-                .sorted((left, right) -> right.getScore() - left.getScore())
+                .sorted((left, right) -> Integer.compare(right.getScore(), left.getScore()))
                 .takeWhile(student -> student.getScore() > bound)
                 .collect(Collectors.toList());
     }
